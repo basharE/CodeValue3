53fc1aa6a2796c41e1fecfc42407f6c41f96ad11
bashar <bashar@DESKTOP-MUJNPLS>
2016-07-30 22:37:01 +0300

    update

---
+++ GenericApp/GenericApp/MultiDictionary.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace GenericApp
+{
+
+    public class MultiDictionary<K, V> : IMultiDictionary<K, V>, IEnumerable<KeyValuePair<K, V>> where V : new()
+    {
+        
+        Dictionary<K, LinkedList<V>> _dic = new Dictionary<K, LinkedList<V>>();
+        
+        public void Add(K key, V value)
+        {
+            var type = key.GetType();
+            var res = type.GetCustomAttribute<KeyAttribute>();
+            if (value != null && null != res)
+            {            
+                if (!_dic.ContainsKey(key))
+                {
+                    LinkedList<V> l = new LinkedList<V>();
+                    l.AddLast(value);
+                    _dic.Add(key, l);
+                }
+                else
+                {
+                    if (!_dic[key].Contains(value))
+                    {
+                        _dic[key].AddLast(value);
+                    }    
+                }
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }             
+        }
+
+
+        public void CreateNewValue(K key)
+        {
+            V newValue = new V();
+            Add(key, (V) newValue);
+        }
+
+
+        public bool Remove(K key)
+        {
+            return _dic.Remove(key);
+        }
+
+
+        public bool Remove(K key, V value)
+        {
+            if (value != null && key != null)
+                return _dic[key].Remove(value);
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+
+        public void Clear()
+        {
+            foreach (KeyValuePair<K, LinkedList<V>> a in _dic)
+            {
+                a.Value.Clear();
+            }
+            _dic.Clear();
+        }
+
+
+        public bool ContainsKey(K key)
+        {
+            return _dic.ContainsKey(key);
+        }
+
+
+        public bool Contains(K key, V value)
+        {
+            if (value!=null)
+            {
+                if (ContainsKey(key))
+                    return _dic[key].Contains(value);
+                else
+                    return false;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+
+        public ICollection<K> Keys
+        {
+            get
+            {
+                LinkedList<K> keys = new LinkedList<K>(_dic.Keys);
+                return keys;
+            }
+        }
+
+
+        public ICollection<V> Values
+        {
+            get
+            {
+                LinkedList<V> keys = new LinkedList<V>();
+                foreach (KeyValuePair<K, LinkedList<V>> a in _dic)
+                {
+                    LinkedList<V> keysB = new LinkedList<V>();
+                    keysB = a.Value;
+                    foreach(var k in keysB)
+                        if (!keys.Contains(k))
+                        {
+                            keys.AddFirst(k);
+                        }
+                }
+                return keys;
+            }
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                int count=0;
+                foreach (KeyValuePair<K, LinkedList<V>> a in _dic)
+                {
+                    count+= a.Value.Count();
+                }
+                return count;
+            }
+        }        
+        
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach (var d in _dic)
+            {               
+                foreach (var v in d.Value)
+                    yield return new KeyValuePair<K, V>(d.Key, v);
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _dic.GetEnumerator();
+        }        
+    }
+
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class KeyAttribute : Attribute
+    {
+    }
+
+}
---
+++ GenericApp/GenericApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GenericApp
+{
+    [Key]
+    public class stam
+    {
+        
+    }
+    class Program
+    {
+        static void Main()
+        {
+            MultiDictionary<stam, char> aDictionary = new MultiDictionary<stam, char>();
+
+            aDictionary.Add(new stam(), 'a');
+            Console.WriteLine(aDictionary.Count);
+            //aDictionary.Add(1, "one");
+            //Console.WriteLine(aDictionary.Count);
+            //aDictionary.CreateNewValue(1);
+            //aDictionary.CreateNewValue(2);
+            //Console.WriteLine(aDictionary.ContainsKey(2));
+            //aDictionary.Add(1, "one");
+            //aDictionary.Add(2, "two");
+            //aDictionary.Add(3, "three");
+            //aDictionary.Add(1, "ich");
+            //aDictionary.Add(2, "nee");
+            //aDictionary.Add(3, "sun");
+            //Console.WriteLine(aDictionary.Count);
+            //aDictionary.Remove(1);
+            //Console.WriteLine(aDictionary.Count);
+            //aDictionary.Remove(2,"two");
+            //Console.WriteLine(aDictionary.Count);
+            //Console.WriteLine(aDictionary.ContainsKey(1));
+            //Console.WriteLine(aDictionary.Contains(2,"nee"));
+            //Console.WriteLine(aDictionary.ContainsKey(2));
+            //Console.WriteLine(aDictionary.ContainsKey(3));
+            //Console.WriteLine(aDictionary.ContainsKey(4));
+
+        }
+    }
+}
